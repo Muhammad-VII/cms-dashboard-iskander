2aa7c48fae09ff9efc04c7312357e288889d78a3
Muhammad Akmal <akmal@emojiit.com>
2022-12-25 15:25:38 +0200

    media edit added

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -17,7 +17,8 @@ export class HomeComponent implements OnDestroy {
   loading = false;
   pageData: any;
   lang: string = localStorage.getItem('lang')! ?? 'due';
-  editForm:FormGroup = new FormGroup({
+  uploadLoading: boolean = true;
+  editForm: FormGroup = new FormGroup({
     _id: new FormControl(''),
     title: new FormControl(''),
     subTitle: new FormControl(''),
@@ -26,15 +27,24 @@ export class HomeComponent implements OnDestroy {
     image: new FormControl(''),
     media: new FormControl(''),
     hidden: new FormControl(''),
-    btnHidden: new FormControl('')
-  })
+    btnHidden: new FormControl(''),
+  });
+
+  editMediaForm: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    subTitle: new FormControl(''),
+    image: new FormControl(''),
+    description: new FormControl(''),
+  });
+
   constructor(
     private _AuthService: AuthService,
     private _DashboardService: DashboardService,
     private _NgxSpinnerService: NgxSpinnerService,
     private _ToastrService: ToastrService
   ) {
-    this.editForm.valueChanges.subscribe(console.log)
+    this.editForm.valueChanges.subscribe(console.log);
+    this.editMediaForm.valueChanges.subscribe(console.log);
     this._NgxSpinnerService.show();
     this.subscribtions.push(
       this._DashboardService.getHomePage().subscribe({
@@ -73,11 +83,11 @@ export class HomeComponent implements OnDestroy {
         error: (err) => {
           this._NgxSpinnerService.hide();
           this._ToastrService.error('An error has occured');
-        }
+        },
       });
   }
 
-  patchValuesToEditForm(obj:any){
+  patchValuesToEditForm(obj: any) {
     this.editForm.patchValue({
       _id: obj._id,
       title: obj.title,
@@ -88,23 +98,40 @@ export class HomeComponent implements OnDestroy {
       media: obj.media,
       hidden: obj.hidden,
       btnHidden: obj.btnHidden,
-    })
-  }
-
-  uploadimage(event: any) {
-    this._DashboardService.uploadImage(event.target.files[0], `images/home/${event.target.files[0].name}`).subscribe({
-      next: (val) => {
-        this.editForm.patchValue({
-          image: val
-        })
-      }, 
-      error: (err) => {
-        console.log(err);
-      }
     });
   }
 
-  submitEditForm(){
+  uploadimage(event: any, type: string) {
+    this.uploadLoading = false;
+    this._DashboardService
+      .uploadImage(
+        event.target.files[0],
+        `images/home/${event.target.files[0].name}`
+      )
+      .subscribe({
+        next: (val) => {
+          if (type === 'data') {
+            this.editForm.patchValue({
+              image: val,
+            });
+          } else if (type === 'media') {
+            this.editMediaForm.patchValue({
+              image: val,
+            });
+          }
+          this.uploadLoading = true;
+        },
+        error: (err) => {
+          this.uploadLoading = true;
+          this._ToastrService.error(
+            'An error has occured while uploading the image'
+          );
+          console.log(err);
+        },
+      });
+  }
+
+  submitEditForm() {
     this._NgxSpinnerService.show();
     this._DashboardService
       .update(this.editForm.value._id, {
@@ -127,7 +154,7 @@ export class HomeComponent implements OnDestroy {
         error: (err) => {
           this._NgxSpinnerService.hide();
           this._ToastrService.error('An error has occured');
-        }
+        },
       });
   }
 
@@ -135,51 +162,95 @@ export class HomeComponent implements OnDestroy {
     this._NgxSpinnerService.show();
     const mediaArray = this.editForm.value.media;
     mediaArray.splice(index, 1);
-    this._DashboardService.update(this.editForm.value._id, {
-      title: this.editForm.value.title,
-      subTitle: this.editForm.value.subTitle,
-      extraTitle: this.editForm.value.extraTitle,
-      description: this.editForm.value.description,
-      image: this.editForm.value.image,
-      media: mediaArray,
-      hidden: this.editForm.value.hidden,
-      dir: this.editForm.value.dir,
-      }).subscribe({
+    this._DashboardService
+      .update(this.editForm.value._id, {
+        title: this.editForm.value.title,
+        subTitle: this.editForm.value.subTitle,
+        extraTitle: this.editForm.value.extraTitle,
+        description: this.editForm.value.description,
+        image: this.editForm.value.image,
+        media: mediaArray,
+        hidden: this.editForm.value.hidden,
+        dir: this.editForm.value.dir,
+      })
+      .subscribe({
+        next: (res) => {
+          this._NgxSpinnerService.hide();
+          this._ToastrService.success('The section has been updated');
+          window.location.reload();
+        },
+      });
+  }
+
+  uploadMedia(event: any, index: number) {
+    this._DashboardService
+      .uploadImage(
+        event.target.files[0],
+        `images/home/${event.target.files[0].name}`
+      )
+      .subscribe({
+        next: (val) => {
+          this._NgxSpinnerService.show();
+          const mediaArray: any[] = this.editForm.value.media;
+          //TODO OPENS MODEL TO EDIT TEXT AS WELL AS IMAGE
+          this._DashboardService
+            .update(this.editForm.value._id, {
+              title: this.editForm.value.title,
+              subTitle: this.editForm.value.subTitle,
+              extraTitle: this.editForm.value.extraTitle,
+              description: this.editForm.value.description,
+              image: this.editForm.value.image,
+              media: mediaArray,
+              hidden: this.editForm.value.hidden,
+              dir: this.editForm.value.dir,
+            })
+            .subscribe({
+              next: (res) => {
+                this._NgxSpinnerService.hide();
+                this._ToastrService.success('The section has been updated');
+                window.location.reload();
+              },
+            });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+  mediaIndex: number = 0;
+  editMedia(obj: any, index: number) {
+    this.editMediaForm.patchValue({
+      title: obj.title,
+      subTitle: obj.subTitle,
+      description: obj.description,
+      image: obj.image,
+    });
+    this.mediaIndex = index;
+  }
+
+  submitEditMedia() {
+    let mediaArray: any[] = this.editForm.value.media;
+    mediaArray[this.mediaIndex] = this.editMediaForm.value;
+    console.log(mediaArray);
+    this._NgxSpinnerService.show();
+    this._DashboardService
+      .update(this.editForm.value._id, {
+        title: this.editForm.value.title,
+        subTitle: this.editForm.value.subTitle,
+        extraTitle: this.editForm.value.extraTitle,
+        description: this.editForm.value.description,
+        image: this.editForm.value.image,
+        media: mediaArray,
+        hidden: this.editForm.value.hidden,
+        btnHidden: this.editForm.value.dir,
+      })
+      .subscribe({
         next: (res) => {
           this._NgxSpinnerService.hide();
           this._ToastrService.success('The section has been updated');
           window.location.reload();
         }
-      })
-  }
-
-  uploadMedia(event: any, index: number) {
-    this._DashboardService.uploadImage(event.target.files[0], `images/home/${event.target.files[0].name}`).subscribe({
-      next: (val) => {
-        this._NgxSpinnerService.show();
-        const mediaArray:any[] = this.editForm.value.media;
-        //TODO OPENS MODEL TO EDIT TEXT AS WELL AS IMAGE
-        this._DashboardService.update(this.editForm.value._id, {
-          title: this.editForm.value.title,
-          subTitle: this.editForm.value.subTitle,
-          extraTitle: this.editForm.value.extraTitle,
-          description: this.editForm.value.description,
-          image: this.editForm.value.image,
-          media: mediaArray,
-          hidden: this.editForm.value.hidden,
-          dir: this.editForm.value.dir,
-          }).subscribe({
-            next: (res) => {
-              this._NgxSpinnerService.hide();
-              this._ToastrService.success('The section has been updated');
-              window.location.reload();
-            }
-          })
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    });
+      });
   }
 
   ngOnDestroy(): void {
