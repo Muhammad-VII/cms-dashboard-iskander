7e7b096045981e3528a4ed72ee617671b4704d3b
Muhammad Akmal <akmal@emojiit.com>
2022-12-26 12:13:18 +0200

    added translation form sidnav links

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { AuthService } from './../../services/auth.service';
 import { Component } from '@angular/core';
-import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +7,7 @@ import { DashboardService } from 'src/app/services/dashboard.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  constructor(public _AuthService: AuthService, private _DashboardService: DashboardService) {}
+  constructor(public _AuthService: AuthService) {}
   lang: string = localStorage.getItem('lang')! ?? 'due';
   isCollapsed: boolean = true;
 
